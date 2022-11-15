5204c8bdfe26081000f5b619a3b7670de18be663
ikguj2023 <116536236+ikguj2023@users.noreply.github.com>
2022-11-15 09:33:46 -0800

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,5 +1,6 @@
 def handler(event, context):
   print("Welcome to lambda handler")
   print("Run your lambda code from here")
+  print("Run your lambda code from here - Nove 15, 9:31 changes")
   
   return{"action": "handler_executed"}
