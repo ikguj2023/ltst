314f73c314e4c3a8380940208ec458cbbd30ead5
ikguj2023 <116536236+ikguj2023@users.noreply.github.com>
2022-11-15 21:19:11 -0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,8 @@
 import sys
+import requests
+
 def handler(event, context): 
+    resp=requests.get("https://www.google.com")
+    print(resp)
     return 'Hello from AWS Lambda using Python' + sys.version + '!'
+
