05f2d56ce09c795a99b2cc2a6111bd775c3dc111
ikguj2023 <116536236+ikguj2023@users.noreply.github.com>
2022-11-22 14:58:11 -0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import sys
 
 def handler(event, context): 
-    print("test at nov 15 10:09 pm")
+    print("new test at nov 15 10:09 pm")
     return 'Hello from AWS Lambda using Python' + sys.version + '!'
 
